446e10caf420eb1b0cb07a4b77913320e881e566
Eddie Zhang <ezhang7423@student.palomar.edu>
2019-12-11 11:21:55 -0800

    remove  print statements

--- scrape.py
+++ scrape.py
@@ -6,10 +6,8 @@ config = Path('./token.txt')
 if config.is_file():
     accessToken = open('token.txt', 'r')
     token = accessToken.read()
-    print(token)
 else:
     token = input('input token:')
-    print(token)
 
     
 g = Github(token)
