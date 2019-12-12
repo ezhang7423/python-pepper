54fbd87c8e774859480cad5999855c74620027bb
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-12-11 21:02:11 -0800

    reformat json

--- scrape.py
+++ scrape.py
@@ -1,10 +1,29 @@
 import requests
-
+from pathlib import Path
 
 
 
 url = 'https://api.github.com/graphql'
-json = { 'query' : '{ viewer { repositories(first: 30) { totalCount pageInfo { hasNextPage endCursor } edges { node { name } } } } }' }
+json = { 'query' : '''{
+  repository(owner:"octocat", name:"Hello-World") {
+    issues(last:20, states:CLOSED) {
+      edges {
+        node {
+          title
+          url
+          labels(first:5) {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}'''
+}
 
 config = Path('./token.txt')
 
@@ -18,4 +37,4 @@ else:
 headers = {'Authorization': 'token {}'.format(token)}
 
 r = requests.post(url=url, json=json, headers=headers)
-print (r.text)
+print(r.text)
