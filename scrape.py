d37d0862d6d9fe103a28bca72fa9bc5e99a98959
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-12-11 21:52:00 -0800

    give up b/c github graphql pagination bug

--- scrape.py
+++ scrape.py
@@ -3,23 +3,13 @@ import ql
 ql.get(
     {
         "query": """{
-    repository(owner:"octocat", name:"Hello-World") {
-        issues(last:20, states:CLOSED) {
-        edges {
-            node {
-            title
-            url
-            labels(first:5) {
-                edges {
-                node {
+    organization(login: "ucsb-cs16-mirza") {
+            membersWithRole {
+                nodes {
                     name
                 }
-                }
-            }
             }
         }
-        }
-    }
     }"""
     }
 )
